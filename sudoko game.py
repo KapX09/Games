3d7cfd486c421b102fd8117cf88107a6c1155fd9
Krishykumari Solanki <139873751+KapX09@users.noreply.github.com>
2026-04-04 02:04:51 +0530

    Update sudoko game.py

--- sudoko game.py
+++ sudoko game.py
@@ -1,47 +1,68 @@
-M= 9
-def puzzle(a):
+M = 9
+
+def print_grid(grid):
+    """Print the Sudoku grid nicely"""
     for i in range(M):
         for j in range(M):
-            print(a[i][j],end = " ")
+            print(grid[i][j], end=" ")
         print()
-def solve(grid, row, col, num):
+
+
+def is_safe(grid, row, col, num):
+    """Check if it's safe to place num at grid[row][col]"""
+    
+    # Check row
     for x in range(9):
         if grid[row][x] == num:
             return False
-             
+    
+    # Check column
     for x in range(9):
         if grid[x][col] == num:
             return False
- 
- 
-    startRow = row - row % 3
-    startCol = col - col % 3
+    
+    # Check 3x3 subgrid
+    start_row = row - row % 3
+    start_col = col - col % 3
     for i in range(3):
         for j in range(3):
-            if grid[i + startRow][j + startCol] == num:
+            if grid[i + start_row][j + start_col] == num:
                 return False
+    
     return True
- 
-def Suduko(grid, row, col):
- 
-    if (row == M - 1 and col == M):
+
+
+def solve_sudoku(grid, row=0, col=0):
+    """Main Sudoku solver using backtracking"""
+    
+    # Base case: we reached the end of the grid
+    if row == M - 1 and col == M:
         return True
+    
+    # Move to next row if we finished current row
     if col == M:
         row += 1
         col = 0
+    
+    # If cell is already filled, move to next cell
     if grid[row][col] > 0:
-        return Suduko(grid, row, col + 1)
-    for num in range(1, M + 1, 1): 
-     
-        if solve(grid, row, col, num):
-         
+        return solve_sudoku(grid, row, col + 1)
+    
+    # Try placing numbers 1 to 9
+    for num in range(1, M + 1):
+        if is_safe(grid, row, col, num):
             grid[row][col] = num
-            if Suduko(grid, row, col + 1):
+            
+            if solve_sudoku(grid, row, col + 1):
                 return True
-        grid[row][col] = 0
+                
+            # Backtrack
+            grid[row][col] = 0
+    
     return False
- 
-'''0 means the cells where no value is assigned'''
+
+
+# Your given puzzle (0 means empty)
 grid = [
     [2, 5, 0, 0, 3, 0, 9, 0, 1],
     [0, 1, 0, 0, 0, 4, 0, 0, 0],
@@ -51,9 +72,15 @@ grid = [
     [0, 4, 0, 0, 0, 3, 0, 0, 0],
     [0, 0, 0, 3, 6, 0, 0, 7, 2],
     [0, 7, 0, 0, 0, 0, 0, 0, 3],
-    [9, 0, 3, 0, 0, 0, 6, 0, 4]]
- 
-if (Suduko(grid, 0, 0)):
-    puzzle(grid)
+    [9, 0, 3, 0, 0, 0, 6, 0, 4]
+]
+
+print("Original Sudoku:")
+print_grid(grid)
+print("\nSolving...\n")
+
+if solve_sudoku(grid):
+    print("Solution found:")
+    print_grid(grid)
 else:
-    print("Solution does not exist:(")
+    print("No solution exists :(")
